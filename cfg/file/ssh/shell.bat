3fb5bb1e3cde83e5d290374d30a765fe611e6fd2
Marshall Porter <meoporter@gmail.com>
2026-01-03 03:50:15 +0000

    windows ssh setup

---
+++ cfg/file/ssh/shell.bat
@@ -0,0 +1,2 @@
+@echo off
+"C:\Program Files\Powershell\7\pwsh.exe" -nologo %*
---
+++ cfg/script/pwsh/setup/ssh.ps1
@@ -0,0 +1,45 @@
+& {
+  if ($IsWindows) {
+    $yn = ''
+    if ($YES) {
+      $yn = 'y'
+    } else {
+      $yn = Read-Host '? setup ssh - disable admin authorized keys file (system) [y, [n]]'
+    }
+    if ($yn -ne 'n') {
+      $filePath = "C:\ProgramData\ssh\sshd_config"
+      $findLines = @(
+        , 'Match Group administrators'
+        , 'AuthorizedKeysFile __PROGRAMDATA__/ssh/administrators_authorized_keys'
+      ).ToLower()
+      $output = @()
+      foreach ($line in Get-Content -Path $filePath) {
+        $o = $null
+        $lineFormatted = $line.Trim().ToLower()
+        foreach ($findLine in $findLines) {
+          if ($lineFormatted -eq $findLine) {
+            $o = "#${line}"
+            break
+          }
+        }
+        if (-not $o) {
+          $o = $line
+        }
+        $output += $o
+      }
+      opPrintMaybeRunCmd sudo pwsh -c "'Set-Content -Path `"${filePath}`" -Value `"${output}`"'"
+    }
+    if ($YES) {
+      $yn = 'y'
+    } else {
+      $yn = Read-Host '? setup ssh - set default shell to pwsh (system) [y, [n]]'
+    }
+    if ($yn -ne 'n') {
+      $path = 'HKLM:\SOFTWARE\OpenSSH'
+      $shell = 'C:\ssh\shell.bat'
+      opPrintMaybeRunCmd sudo pwsh -c "'New-ItemProperty `"${path}`" -Name DefaultShell -Value `"${shell}`" -PropertyType String -Force'"
+    }
+  } else {
+    Write-Host 'script is for winnt'
+  }
+}
